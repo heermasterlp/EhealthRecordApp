3af2f779c61b36df2a4ed737e6cb7a2443521ea7
heermaster <heermaster@heermasterdeMacBook-Pro.local>
2015-04-21 22:18:42 +0800

    Initial Commit

---
+++ EhealthApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EhealthApp
+//
+//  Created by heermaster on 15/4/21.
+//  Copyright (c) 2015年 heermaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
